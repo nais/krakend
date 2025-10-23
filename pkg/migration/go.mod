18d1d8d6c4d10d99c320e9d4595232f8177e3477
frodesundby <frode.sundby@nav.no>
2025-10-23 14:25:25 +0200

    feat: remove verify to avoid go-cmp incompatability issues
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>

--- pkg/migration/go.mod
+++ pkg/migration/go.mod
@@ -3,7 +3,6 @@ module github.com/nais/krakend/pkg/migration
 go 1.24.6
 
 require (
-	github.com/google/go-cmp v0.7.0
 	github.com/nais/krakend v0.0.0-20251023101753-7caa0215c6b9
 	github.com/nais/liberator v0.0.0-20250924103433-536eaed90405
 	github.com/sirupsen/logrus v1.9.3
@@ -28,6 +27,7 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -67,6 +67,6 @@ require (
 	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.6.0 // indirect
-	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.2.0 // indirect
 	sigs.k8s.io/yaml v1.6.0 // indirect
 )
--- pkg/migration/parse/config.go
+++ pkg/migration/parse/config.go
@@ -3,7 +3,7 @@ package parse
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/google/go-cmp/cmp"
+
 	krakendv1 "github.com/nais/krakend/api/v1"
 	"github.com/nais/krakend/internal/krakend"
 	corev1 "k8s.io/api/core/v1"
@@ -12,24 +12,6 @@ import (
 const (
 	ConfigMapEndpointsKey = "endpoints.json"
 )
-
-func VerifyPartialsConfig(new *corev1.ConfigMap, old *corev1.ConfigMap) error {
-	newEndpoints := new.Data[ConfigMapEndpointsKey]
-	oldEndpoints := old.Data["endpoints.tmpl"]
-	want, err := krakend.ParsePartials([]byte(oldEndpoints))
-	if err != nil {
-		return fmt.Errorf("unmarshalling existing partials configmap json: %v", err)
-	}
-	got, err := krakend.ParsePartials([]byte(newEndpoints))
-	if err != nil {
-		return fmt.Errorf("unmarshalling generated partials configmap json: %v", err)
-	}
-
-	if diff := cmp.Diff(want, got); diff != "" {
-		return fmt.Errorf("diff in config map: %s", diff)
-	}
-	return nil
-}
 
 func ToKrakendConfig(k *krakendv1.Krakend) (*corev1.ConfigMap, error) {
 	cm := &corev1.ConfigMap{}
